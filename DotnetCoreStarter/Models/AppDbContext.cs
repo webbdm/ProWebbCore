b30e1eea6cea147c36df9f3b6ccec843e1d803d7
Geoff Webb <gwebb@atiba.com>
2020-04-11 00:09:03 -0500

    Initial setup

---
+++ DotnetCoreStarter/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotnetCoreStarter.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
