256090bf6d4b9a6242beed1a52ecc3c6e0475b34
Geoff Webb <gwebb762@gmail.com>
2020-05-31 01:23:50 -0500

    Docker & webpack configurations

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,1 +1,13 @@
-console.log('Hello Webpack Project.');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const title = 'React with Webpack and Babel';
+
+ReactDOM.render(
+    <App title={title} />,
+    document.getElementById('app')
+);
+
+module.hot.accept();
--- Client/webpack.config.js
+++ Client/webpack.config.js
@@ -1,14 +1,29 @@
+const webpack = require('webpack');
+
 module.exports = {
-    // 1
     entry: './src/index.js',
-    // 2
-    output: {
-      path: __dirname + '/dist',
-      publicPath: '/',
-      filename: 'bundle.js'
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
     },
-    // 3
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
     devServer: {
-      contentBase: './dist'
+        contentBase: './dist',
+        hot: true 
     }
-  };
+};
