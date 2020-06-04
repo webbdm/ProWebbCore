85e44f7b44650b1669a7f9ad4b4d68e822b73d2e
Geoff Webb <gwebb762@gmail.com>
2020-06-03 20:24:44 -0500

    Add AWS.s3 package and modified Startup Configuration syntax

--- ProWebbCore/ProWebbCore.Api/Startup.cs
+++ ProWebbCore/ProWebbCore.Api/Startup.cs
@@ -5,14 +5,20 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using ProWebbCore.Api.Models;
+using ProWebbCore.Infrastructure.Repositories;
+using ProWebbCore.Infrastructure.Communication.Interfaces;
+using Amazon.S3;
+
 
 namespace ProWebbCore.Api
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -20,8 +26,16 @@ namespace ProWebbCore.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(options => options.UseMySql((Configuration.GetConnectionString("DatabaseConnectionString"))));
-           
+            services.AddDbContext<AppDbContext>(options => options.UseMySql((_configuration.GetConnectionString("DatabaseConnectionString"))));
+            
+            services.AddAWSService<IAmazonS3>(_configuration.GetAWSOptions());
+            services.AddSingleton<IBucketRepository, BucketRepository>();
+            services.AddSingleton<IFilesRepository, FilesRepository>();
+
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+
+
+
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IJobRepository, JobRepository>();
             services.AddScoped<ISkillRepository, SkillRepository>();
@@ -39,6 +53,8 @@ namespace ProWebbCore.Api
             }
 
             // context.Database.Migrate(); // Not always needed
+
+            app.UseMvc();
 
             app.UseRouting();
 
