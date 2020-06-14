edc00567c3a3ed18600153bc44cfc6777ef5af76
Geoff Webb <gwebb762@gmail.com>
2020-06-14 17:27:05 -0500

    nginc config

--- Client/webpack.config.js
+++ Client/webpack.config.js
@@ -25,6 +25,7 @@ module.exports = {
     devServer: {
         contentBase: './dist',
         hot: true,
-        historyApiFallback: true 
+        historyApiFallback: true,
+        disableHostCheck: true 
     }
 };
