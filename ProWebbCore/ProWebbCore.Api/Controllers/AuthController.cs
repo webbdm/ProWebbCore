9179b2fda09c77ce46bb30af0622addc90c3c6c4
Geoff Webb <gwebb762@gmail.com>
2020-11-28 23:28:25 -0600

    PWC-16 Created authcontroller and access token

---
+++ ProWebbCore/ProWebbCore.Api/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProWebbCore.Shared.Auth;
+
+namespace ProWebbCore.Api.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [Route("/callback")]
+        [HttpPost]
+        public IActionResult Callback(AccessToken token)
+        {
+            // Create the access token record in the database
+
+            // Send a response
+            // This should then redirect user from auth with valid access token
+
+            return Ok();
+        }
+    }
+}
---
+++ ProWebbCore/ProWebbCore.Shared/Auth/AccessToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProWebbCore.Shared.Auth
+{
+    public class AccessToken
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ClientId { get; set; }
+    }
+}
