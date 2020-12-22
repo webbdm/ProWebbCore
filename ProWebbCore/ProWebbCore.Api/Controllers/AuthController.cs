401cd64db84697653d5ad32a7ee5eb709233db4a
webbdm <gwebb762@gmail.com>
2020-12-21 20:34:14 -0600

    PWC-000 Created AuthController

---
+++ ProWebbCore/ProWebbCore.Api/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using ProWebbCore.Api.Models;
+using ProWebbCore.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ProWebbCore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+
+        [HttpPost]
+        [Route("/auth/callback")]
+        public bool Callback()
+        {
+            return true;
+        }
+    }
+}
